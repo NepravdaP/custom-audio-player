a8b606abb47275c4b92edda29a7e28435bbdcc7e
Pavel Harmaza <91322286+NepravdaP@users.noreply.github.com>
2022-01-31 23:08:10 +0300

    feat: add structure and play button

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log("test");
+const audio = document.querySelector("audio");
+const bgCover = document.querySelector(".bg-cover");
+const cover 
+const playBtn = document.querySelector(".play-pause");
+let isPlaying = false;
+const playPause = () => {
+  if (isPlaying === false) {
+    audio.play();
+    isPlaying = true;
+    playBtn.src = "./assets/svg/pause.png";
+  } else if (isPlaying === true) {
+    audio.pause();
+    isPlaying = false;
+    playBtn.src = "./assets/svg/play.png";
+  }
+};
