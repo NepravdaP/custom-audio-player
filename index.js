5ba1727fe4653bb47f1a26a80aade88abd037451
Pavel Harmaza <91322286+NepravdaP@users.noreply.github.com>
2022-02-01 03:41:33 +0300

    feat: progress bar functional

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const cover = document.querySelector(".thumbnail");
 const playBtn = document.querySelector(".play-pause");
 const artist = document.querySelector(".artist-name");
 const title = document.querySelector(".song-title");
+const currTime = document.querySelector(".currentTime");
+const durationTime = document.querySelector(".durationTime");
+const progressBar = document.querySelector("input");
 const titleColorArr = ["#4f1a20", "#1C4242", "#F4EA77"];
 const CoverSrcArr = [
   "./assets/img/CIA.jpg",
@@ -20,6 +23,33 @@ const songTitleArr = ["CIA", "Don't Hurt Yourself", "Don't Start Now"];
 const songArtistArr = ["New Politics", "Beyonce", "Dua Lipa"];
 let currSong = 0;
 let isPlay = false;
+audio.volume = 0.2;
+// const durInterval = () => {
+//   while (audio.currentTime != audio.duration) {
+//     const interval = setInterval(() => {
+//       console.log(audio.currentTime);
+//     }, 1000);
+//     if (audio.currentTime === audio.duration) {
+//       clearInterval(interval);
+//     }
+//   }
+// };
+
+// audio.addEventListener("loadeddata", () => {
+//   currTime.textContent = audio.currentTime;
+//   durationTime.textContent = audio.duration;
+// });
+progressBar.addEventListener("change", (e) => {
+  audio.currentTime = e.target.value;
+});
+audio.addEventListener("timeupdate", () => {
+  const minutes = Math.floor(audio.currentTime / 60);
+  const seconds = Math.floor(audio.currentTime - minutes * 60);
+  currTime.textContent = `${minutes}:${seconds < 10 ? "0" + seconds : seconds}`;
+  const progress = (audio.duration / 100) * audio.currentTime;
+  progressBar.max = audio.duration;
+  progressBar.value = audio.currentTime;
+});
 const songAssets = () => {
   artist.textContent = songArtistArr[currSong];
   title.textContent = songTitleArr[currSong];
@@ -29,18 +59,20 @@ const songAssets = () => {
   artist.style = `color:${titleColorArr[currSong]}`;
 };
 const playPause = () => {
-  if (isPlay === false) {
+  if (!isPlay) {
     audio.play();
     isPlay = true;
     playBtn.src = "./assets/svg/pause.png";
     cover.style = "transform: scale(1.15)";
-  } else if (isPlay === true) {
+    audio.volume = 0.2;
+  } else if (isPlay) {
     audio.pause();
     isPlay = false;
     playBtn.src = "./assets/svg/play.png";
     cover.style = "transform: scale(1)";
   }
 };
+
 const playNext = () => {
   if (currSong === 2) {
     currSong = 0;
@@ -58,8 +90,12 @@ const playPrev = () => {
   } else {
     currSong--;
   }
-  audio.src = songSrcArr[currSong];
+  songAssets();
+
   audio.currentTime = 0;
   audio.play();
   playBtn.src = "./assets/svg/pause.png";
 };
+
+console.log(audio.currentTime);
+console.log(audio.duration);
